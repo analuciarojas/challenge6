0190e3584230f5499d564b83ce341b198186fa93
AnaLucia Rojas <analuciarojas@AnaLucias-MacBook-Air.local>
2021-11-07 23:25:47 -0800

    city lists done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 var currentDay=document.querySelector("#currentDay");
-
+var cityArray = [];
+var cityList =$("#city-list");
+var line=$("#hr");
 
 
 setInterval(function () {
@@ -8,4 +10,35 @@ setInterval(function () {
      );
    },1000);
 
-   
+function saveCity(){
+    localStorage.setItem("cities", JSON.stringify(cityArray));
+}
+
+function displayCity(){
+    cityList.empty();
+    
+    for (var i = 0; i < cityArray.length; i++) {
+      var city = cityArray[i];
+      
+      var button = $("<button>").text(city);
+      button.attr("class", "list-group-item", "list-group-item active");
+      button.attr("aria-current", "true");
+      cityList.prepend(button);
+    }
+    
+}
+
+ $("#search-city").on("click", function(){
+    var hr = document.createElement("HR");
+    var city = $("#city").val().trim();
+
+    if(cityArray.length===0){
+        var hr = $("<hr>");
+        line.prepend(hr);
+    }
+    if (city !== "") {
+        cityArray.push(city);
+    }
+    saveCity();
+    displayCity();
+});
